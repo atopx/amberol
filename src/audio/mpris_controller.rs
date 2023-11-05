cc571a8c452e78a59fc73559c3be38f177b7d0e6
Arnaud Ferraris <aferraris@debian.org>
2023-11-05 14:23:23 +0100

    mpris_controller: replace "mpris_player" with "mpris_server"
    The `mpris-player` crate has been deprecated upstream in July 2022. The
    best alternative currently is `mpris-server`, which is actively
    maintained, published on crates.io and has an API very similar to the
    one from `mpris-player`.
    
    The main differences are the following:
    * the `Player` object, once created, must be initialized and run in an
      async block, requiring a new task to be spawned
    * most setters are async as well, with the same side effect (spawn new
      tasks)
    
    This change is a straightforward port, partly inspired (for the async
    operations) by Mousai.

--- src/audio/mpris_controller.rs
+++ src/audio/mpris_controller.rs
@@ -1,13 +1,13 @@
 // SPDX-FileCopyrightText: 2022  Emmanuele Bassi
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use std::{cell::RefCell, sync::Arc, time::Duration};
+use std::{cell::RefCell, rc::Rc};
 
 use async_channel::Sender;
 use glib::clone;
 use gtk::{gio, glib, prelude::*};
 use log::error;
-use mpris_player::{LoopStatus, Metadata, MprisPlayer, OrgMprisMediaPlayer2Player, PlaybackStatus};
+use mpris_server::{LoopStatus, Metadata, PlaybackStatus, Player, Time};
 
 use crate::{
     audio::{Controller, PlaybackAction, PlaybackState, RepeatMode, Song},
@@ -17,26 +17,33 @@ use crate::{
 #[derive(Debug)]
 pub struct MprisController {
     sender: Sender<PlaybackAction>,
-    mpris: Arc<MprisPlayer>,
+    mpris: Rc<Player>,
 
     song: RefCell<Option<Song>>,
 }
 
 impl MprisController {
     pub fn new(sender: Sender<PlaybackAction>) -> Self {
-        let mpris = MprisPlayer::new(
-            APPLICATION_ID.to_string(),
-            "Amberol".to_string(),
-            APPLICATION_ID.to_string(),
+        let mpris = Rc::new(
+            Player::builder(APPLICATION_ID)
+                .identity("Amberol")
+                .desktop_entry(APPLICATION_ID)
+                .can_raise(true)
+                .can_play(false)
+                .can_pause(true)
+                .can_seek(true)
+                .can_go_next(true)
+                .can_go_previous(true)
+                .can_set_fullscreen(false)
+                .build(),
         );
 
-        mpris.set_can_raise(true);
-        mpris.set_can_play(false);
-        mpris.set_can_pause(true);
-        mpris.set_can_seek(true);
-        mpris.set_can_go_next(true);
-        mpris.set_can_go_previous(true);
-        mpris.set_can_set_fullscreen(false);
+        let mpris_task = mpris.init_and_run();
+        glib::spawn_future_local(async move {
+            if let Err(err) = mpris_task.await {
+                error!("Failed to run MPRIS server: {:?}", err);
+            }
+        });
 
         let res = Self {
             sender,
@@ -50,15 +57,15 @@ impl MprisController {
     }
 
     fn setup_signals(&self) {
-        self.mpris.connect_play_pause(
-            clone!(@weak self.mpris as mpris, @strong self.sender as sender => move || {
-                match mpris.get_playback_status().unwrap().as_ref() {
-                    "Paused" => {
+        self.mpris
+            .connect_play_pause(clone!(@strong self.sender as sender => move |mpris| {
+                match mpris.playback_status() {
+                    PlaybackStatus::Paused => {
                         if let Err(e) = sender.send_blocking(PlaybackAction::Play) {
                             error!("Unable to send Play: {e}");
                         }
                     },
-                    "Stopped" => {
+                    PlaybackStatus::Stopped => {
                         if let Err(e) = sender.send_blocking(PlaybackAction::Stop) {
                             error!("Unable to send Stop: {e}");
                         }
@@ -69,53 +76,52 @@ impl MprisController {
                         }
                     },
                 };
-            }),
-        );
+            }));
 
         self.mpris
-            .connect_play(clone!(@strong self.sender as sender => move || {
+            .connect_play(clone!(@strong self.sender as sender => move |_| {
                 if let Err(e) = sender.send_blocking(PlaybackAction::Play) {
                     error!("Unable to send Play: {e}");
                 }
             }));
 
         self.mpris
-            .connect_stop(clone!(@strong self.sender as sender => move || {
+            .connect_stop(clone!(@strong self.sender as sender => move |_| {
                 if let Err(e) = sender.send_blocking(PlaybackAction::Stop) {
                     error!("Unable to send Stop: {e}");
                 }
             }));
 
         self.mpris
-            .connect_pause(clone!(@strong self.sender as sender => move || {
+            .connect_pause(clone!(@strong self.sender as sender => move |_| {
                 if let Err(e) = sender.send_blocking(PlaybackAction::Pause) {
                     error!("Unable to send Pause: {e}");
                 }
             }));
 
         self.mpris
-            .connect_previous(clone!(@strong self.sender as sender => move || {
+            .connect_previous(clone!(@strong self.sender as sender => move |_| {
                 if let Err(e) = sender.send_blocking(PlaybackAction::SkipPrevious) {
                     error!("Unable to send SkipPrevious: {e}");
                 }
             }));
 
         self.mpris
-            .connect_next(clone!(@strong self.sender as sender => move || {
+            .connect_next(clone!(@strong self.sender as sender => move |_| {
                 if let Err(e) = sender.send_blocking(PlaybackAction::SkipNext) {
                     error!("Unable to send SkipNext: {e}");
                 }
             }));
 
         self.mpris
-            .connect_raise(clone!(@strong self.sender as sender => move || {
+            .connect_raise(clone!(@strong self.sender as sender => move |_| {
                 if let Err(e) = sender.send_blocking(PlaybackAction::Raise) {
                     error!("Unable to send Raise: {e}");
                 }
             }));
 
-        self.mpris
-            .connect_loop_status(clone!(@strong self.sender as sender => move |status| {
+        self.mpris.connect_set_loop_status(
+            clone!(@strong self.sender as sender => move |_, status| {
                 let mode = match status {
                     LoopStatus::None => RepeatMode::Consecutive,
                     LoopStatus::Track => RepeatMode::RepeatOne,
@@ -125,11 +131,12 @@ impl MprisController {
                 if let Err(e) = sender.send_blocking(PlaybackAction::Repeat(mode)) {
                     error!("Unable to send Repeat({mode}): {e}");
                 }
-            }));
+            }),
+        );
 
         self.mpris
-            .connect_seek(clone!(@strong self.sender as sender => move |position| {
-                let pos = Duration::from_micros(position as u64).as_secs();
+            .connect_seek(clone!(@strong self.sender as sender => move |_, position| {
+                let pos = position.as_secs().unsigned_abs();
 
                 if let Err(e) = sender.send_blocking(PlaybackAction::Seek(pos)) {
                     error!("Unable to send Seek({pos}): {e}");
@@ -141,12 +148,12 @@ impl MprisController {
         let mut metadata = Metadata::new();
 
         if let Some(song) = self.song.take() {
-            metadata.artist = Some(vec![song.artist()]);
-            metadata.title = Some(song.title());
-            metadata.album = Some(song.album());
+            metadata.set_artist(Some(vec![song.artist()]));
+            metadata.set_title(Some(song.title()));
+            metadata.set_album(Some(song.album()));
 
-            let length = Duration::from_secs(song.duration()).as_micros() as i64;
-            metadata.length = Some(length);
+            let length = Time::from_secs(song.duration() as i64);
+            metadata.set_length(Some(length));
 
             // MPRIS should really support passing a bytes buffer for
             // the cover art, instead of requiring this ridiculous
@@ -159,28 +166,39 @@ impl MprisController {
                     gio::Cancellable::NONE,
                 ) {
                     Ok(info) if info.file_type() == gio::FileType::Regular => {
-                        metadata.art_url = Some(file.uri().to_string());
+                        metadata.set_art_url(Some(file.uri()));
                     }
-                    _ => metadata.art_url = None,
+                    _ => metadata.set_art_url(None::<String>),
                 }
             }
 
             self.song.replace(Some(song));
         }
 
-        self.mpris.set_metadata(metadata);
+        glib::spawn_future_local(clone!(@weak self.mpris as mpris => async move {
+            if let Err(err) = mpris.set_metadata(metadata).await {
+                error!("Unable to set MPRIS metadata: {err:?}");
+            }
+        }));
     }
 }
 
 impl Controller for MprisController {
     fn set_playback_state(&self, state: &PlaybackState) {
-        self.mpris.set_can_play(true);
-
-        match state {
-            PlaybackState::Playing => self.mpris.set_playback_status(PlaybackStatus::Playing),
-            PlaybackState::Paused => self.mpris.set_playback_status(PlaybackStatus::Paused),
-            _ => self.mpris.set_playback_status(PlaybackStatus::Stopped),
+        let status = match state {
+            PlaybackState::Playing => PlaybackStatus::Playing,
+            PlaybackState::Paused => PlaybackStatus::Paused,
+            _ => PlaybackStatus::Stopped,
         };
+
+        glib::spawn_future_local(clone!(@weak self.mpris as mpris => async move {
+            if let Err(err) = mpris.set_can_play(true).await {
+                error!("Unable to set MPRIS play capability: {err:?}");
+            }
+            if let Err(err) = mpris.set_playback_status(status).await {
+                error!("Unable to set MPRIS playback status: {err:?}");
+            }
+        }));
     }
 
     fn set_song(&self, song: &Song) {
@@ -189,15 +207,21 @@ impl Controller for MprisController {
     }
 
     fn set_position(&self, position: u64) {
-        let msecs = Duration::from_secs(position).as_micros();
-        self.mpris.set_position(msecs as i64);
+        let pos = Time::from_secs(position as i64);
+        self.mpris.set_position(pos);
     }
 
     fn set_repeat_mode(&self, repeat: RepeatMode) {
-        match repeat {
-            RepeatMode::Consecutive => self.mpris.set_loop_status(LoopStatus::None),
-            RepeatMode::RepeatOne => self.mpris.set_loop_status(LoopStatus::Track),
-            RepeatMode::RepeatAll => self.mpris.set_loop_status(LoopStatus::Playlist),
-        }
+        let status = match repeat {
+            RepeatMode::Consecutive => LoopStatus::None,
+            RepeatMode::RepeatOne => LoopStatus::Track,
+            RepeatMode::RepeatAll => LoopStatus::Playlist,
+        };
+
+        glib::spawn_future_local(clone!(@weak self.mpris as mpris => async move {
+            if let Err(err) = mpris.set_loop_status(status).await {
+                error!("Unable to set MPRIS loop status: {err:?}");
+            }
+        }));
     }
 }
